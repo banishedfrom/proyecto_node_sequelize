01bc5ed5ae9091db78d90a46e674fdc82ab03144
banished_from_git <danielardila02@gmail.com>
2020-06-02 17:39:12 -0500

    solucion del tercer

--- server/index.js
+++ server/index.js
@@ -48,5 +48,5 @@ app.use("/", routes());
 const HOST = process.env.HOST || "0.0.0.0";
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, HOST, () => {
-  console.log(`Our app is running on port ${PORT}`);
+  console.log(`Our app is running on port ${PORT}, ${HOST}`);
 });
